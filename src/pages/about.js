5446ad9ab82982d7abb4ff719e46637e5a462318
Ben Ronda <bendronda@gmail.com>
2020-09-23 23:55:03 -0700

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div style={{color: "navy"}}>
+      <h1>About Gatsby</h1>
+      <p>Such wow. Very react.</p>
+    </div>
+  )
+}
